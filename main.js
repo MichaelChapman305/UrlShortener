d11d2747af85b1e32f0a082d0e10b87030ae7144
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-10 22:07:58 -0500

    starts js functionality in main.js, creates layout page and adds html to index, and shorten pug pages

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-main.js
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  console.log(req.protocol + '://' + req.get('host') + req.originalUrl);
+  res.render('index');
+});
+
+app.get('/shorten', (req, res) => {
+  res.render('shorten');
+});
+
+app.listen(process.env.PORT || 5000);
