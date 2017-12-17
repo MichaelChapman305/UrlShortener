6edd95f1a203518318b0a67de812b44f165252e4
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-16 22:34:45 -0500

    creates a working URL shortener, if url consists of a website, it will shorten that URL, if the url contains 4 numbers, the browser will redirect to the website of that shortened url, else if the format of the website is not valid, or the numbers cannot be found in the database, an error will be returned

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const jsonParser = require('body-parser').json;
 const app = express();
+
+app.use(jsonParser());
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  console.log(req.protocol + '://' + req.get('host') + req.originalUrl);
-  res.render('index');
+mongoose.connect('mongodb://localhost:27017/url-shortener', {
+  useMongoClient: true
 });
 
-app.get('/shorten', (req, res) => {
-  res.render('shorten');
+const db = mongoose.connection;
+mongoose.Promise = global.Promise;
+
+app.use(routes);
+
+db.on('error', function(err) {
+  console.error('connection error:', err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+  	error: {
+  	  message: err.message
+  	}
+  });
 });
 
 app.listen(process.env.PORT || 5000);
---
+++ routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Url = require('./models').Url;
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/shorten/:url(*)', (req, res, next) => {
+  if (/([0-9][0-9][0-9][0-9])/g.test(req.params.url)) {
+    Url.findOne({'shortened': req.params.url}, (err, doc) => {
+      if(doc === null) {
+        res.send('Url cannot be found!');
+      }else {
+        res.redirect(doc.original); 
+      }
+    });
+  }
+  else if (req.params.url.slice(0, 12) === 'https://www.' && req.params.url.indexOf('.') !== req.params.url.lastIndexOf('.')){
+    req.originalUrl = req.params.url;
+    req.shortUrl = Math.floor(Math.random() * (9999 - 1000) + 1000);
+
+    Url.findOne({'shortened': req.shortUrl}, (err, doc) => {
+      if (doc) {
+        res.redirect('/shorten/:url(*)');
+      }else {
+        let newUrl = new Url({
+          'original': req.originalUrl,
+          'shortened': req.shortUrl
+        });
+        newUrl.save((err, url) => {
+        res.send({'original': req.originalUrl, 'shortened': req.shortUrl});
+        });
+      } 
+    });
+  }else {
+    res.send('Invalid Url. Url must start with "https://www.", and end with ".com" or another valid url ending');
+  }
+});
+
+module.exports = router;
+
