9f8859ace1a9668527bfcfcb32fee60e5e067c87
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-17 16:50:27 -0500

    makes changes to syntax

--- routes.js
+++ routes.js
@@ -9,31 +9,31 @@ router.get('/', (req, res) => {
 router.get('/shorten/:url(*)', (req, res, next) => {
   if (/([0-9][0-9][0-9][0-9])/g.test(req.params.url)) {
     Url.findOne({'shortened': req.params.url}, (err, doc) => {
-      if(!doc) {
+      if (!doc) {
         res.send('Url cannot be found!');
-      }else {
+      } else {
         res.redirect(doc.original); 
       }
     });
   }
   else if (req.params.url.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi) && req.params.url.indexOf('.') !== req.params.url.lastIndexOf('.')){
-    req.originalUrl = req.params.url;
-    req.shortUrl = Math.floor(Math.random() * (9999 - 1000) + 1000);
+    const originalUrl = req.params.url;
+    const shortUrl = Math.floor(Math.random() * (9999 - 1000) + 1000);
 
-    Url.findOne({'shortened': req.shortUrl}, (err, doc) => {
+    Url.findOne({'shortened': shortUrl}, (err, doc) => {
       if (doc) {
         res.redirect('/shorten/:url(*)');
-      }else {
-        let newUrl = new Url({
-          'original': req.originalUrl,
-          'shortened': req.shortUrl
+      } else {
+        const newUrl = new Url({
+          'original': originalUrl,
+          'shortened': shortUrl
         });
         newUrl.save((err, url) => {
-        res.send({'original': req.originalUrl, 'shortened': req.shortUrl});
+          res.send({'original': originalUrl, 'shortened': shortUrl});
         });
       } 
     });
-  }else {
+  } else {
     res.send('Invalid Url. Url must start with "https://www.", and end with ".com" or another valid url ending');
   }
 });
