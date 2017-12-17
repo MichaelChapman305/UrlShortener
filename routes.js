c6513286f6da6c080ca0045ccb68f36536c7dc5c
Michael A. Chapman <chapmanm305@gmail.com>
2017-12-17 16:38:42 -0500

    Changes the url string slice in the if else statement that checks if url is valid to a regular expression

--- routes.js
+++ routes.js
@@ -9,14 +9,14 @@ router.get('/', (req, res) => {
 router.get('/shorten/:url(*)', (req, res, next) => {
   if (/([0-9][0-9][0-9][0-9])/g.test(req.params.url)) {
     Url.findOne({'shortened': req.params.url}, (err, doc) => {
-      if(doc === null) {
+      if(!doc) {
         res.send('Url cannot be found!');
       }else {
         res.redirect(doc.original); 
       }
     });
   }
-  else if (req.params.url.slice(0, 12) === 'https://www.' && req.params.url.indexOf('.') !== req.params.url.lastIndexOf('.')){
+  else if (req.params.url.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi) && req.params.url.indexOf('.') !== req.params.url.lastIndexOf('.')){
     req.originalUrl = req.params.url;
     req.shortUrl = Math.floor(Math.random() * (9999 - 1000) + 1000);
 
